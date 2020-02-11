d86bf356228cd89b9b0cca5dd46fec43673d629f
Steve Archibald <stevearchi21@gmail.com>
2020-02-11 16:40:01 -0700

    random numbers are now generated

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $("#randButton").on("click", function () {
-    $("randNumber").html("");
+    $("#randNumber").empty();
+    for (x=0; x<9; x++) {
+        let randNum = (Math.floor(Math.random()*10))
+        $("#randNumber").append(randNum, " ");
+    }
+
 })
 
